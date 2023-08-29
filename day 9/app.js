db3b2e8cd89d2e1d98dab8a487ab3fcae410a978
Siddharth <siddu2003oct@gmail.com>
2023-08-29 23:18:27 +0530

    creating elems

--- day 9/app.js
+++ day 9/app.js
@@ -1,11 +1,47 @@
-nott = document.querySelector(".list-items") 
-console.log(nott)
-// console.log(document.querySelector(".list-items"))
+// accessing elements
 
+// nott = document.querySelector(".list-items") 
+// console.log(nott)
+// // console.log(document.querySelector(".list-items"))
+
+// const title = document.getElementById('heading');
+// console.log(title)
+
+// console.log("hello")
+// console.log(document.innerHtml)
+// console.log(title.innerHTML)
+
+
+
+
+
+// styling elements
 const title = document.getElementById('heading');
-console.log(title)
+title.style.color = "white"
 
-console.log("hello")
-console.log(document.innerHtml)
-console.log(title.innerHTML)
 
+// const listItems = document.querySelectorAll('.list-items')
+// console.log(listItems)
+
+// console.log(typeof(listItems))
+
+// listItems[2].style.color = 'blue'
+
+
+
+
+
+// creting elements
+unL = document.querySelector('ul')
+newItem = document.createElement('li')
+
+unL.append(newItem)
+
+newItem.innerText = 'Mission Impossible'
+
+newItem.setAttribute('class', 'list-items')
+newItem.removeAttribute('class')
+
+console.log(newItem.classList)
+
+newItem.remove()
