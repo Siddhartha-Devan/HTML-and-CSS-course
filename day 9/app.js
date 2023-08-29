c8b68e2d29b0e19902ff6499536e662fc6b534bc
Siddharth <siddu2003oct@gmail.com>
2023-08-29 22:26:01 +0530

    mudiyala

---
+++ day 9/app.js
@@ -0,0 +1,11 @@
+nott = document.querySelector(".list-items") 
+console.log(nott)
+// console.log(document.querySelector(".list-items"))
+
+const title = document.getElementById('heading');
+console.log(title)
+
+console.log("hello")
+console.log(document.innerHtml)
+console.log(title.innerHTML)
+
