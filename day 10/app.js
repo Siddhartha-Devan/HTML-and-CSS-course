bf69d11b2f9a549d67d15528f31d911a844e3f39
Siddharth <siddu2003oct@gmail.com>
2023-09-01 22:43:39 +0530

    click

--- day 10/app.js
+++ day 10/app.js
@@ -3,3 +3,16 @@ function f1(){
 }
 
 f1()
+
+
+const backgroundColorChanger = document.querySelectorAll('.boxes')
+
+function colorChanger(event){
+    this.style.backgroundColor = 'rgb(241, 152, 8)'
+}
+
+
+backgroundColorChanger.forEach( function(element) {
+    element.addEventListener('click', colorChanger)
+})
+
