4578591cebe93a30935a76c3ae39b77e6570a52c
Siddharth <siddu2003oct@gmail.com>
2023-09-02 23:35:37 +0530

    making the reveal more button with flex

--- day 11/app.js
+++ day 11/app.js
@@ -3,3 +3,25 @@ function dummy(){
 }
 
 
+dummy()
+
+const buttonShower = document.querySelector('.boxes')
+
+const contentShower = document.querySelector('.sub-title')
+
+function changer(){
+    if(contentShower.classList.contains('hidden-cont')){
+        contentShower.classList.remove('hidden-cont')
+        buttonShower.innerHTML = 'Reveal less'
+    }else{
+        contentShower.classList.add('hidden-cont')
+        buttonShower.innerHTML = 'Reveal More'
+        
+    }
+}
+
+
+buttonShower.addEventListener('click', changer)
+
+
+
