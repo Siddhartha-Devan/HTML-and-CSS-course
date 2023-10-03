876d101507a0a4a10a39554e3b28c9101ae5ac9c
Siddharth <siddu2003oct@gmail.com>
2023-10-03 23:45:02 +0530

    done

--- day 12/app.js
+++ day 12/app.js
@@ -4,3 +4,18 @@ console.log("hello")
 //     console.log("football is clicked");
     
 // })
+
+
+
+document.querySelector('#sport').addEventListener('click', function(e){
+    console.log(e.target.getAttribute('id') + ' is clicked.' )     
+
+    const targ = e.target
+
+    if (targ.style.backgroundColor === 'gray'){
+        targ.style.backgroundColor = 'antiquewhite'
+    }else{
+        targ.style.backgroundColor = 'gray'
+    }
+
+})
